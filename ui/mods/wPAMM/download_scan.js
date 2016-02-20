5e68ea0aabe38579bfc0dd0f82071c5d344dabe6
Justin Love <git@JustinLove.name>
2016-02-19 19:59:13 -0600

    detect zips with extra dirs

--- ui/mods/wPAMM/download_scan.js
+++ ui/mods/wPAMM/download_scan.js
@@ -27,13 +27,19 @@ define(['pamm/file'], function(file) {
       my.pending++
       file.zip.read('coui://download/'+path).then(function(zip) {
         //console.log('zip', zip)
-        var infos = zip.file(/modinfo.json$/)
+        var infos = zip.file(/^\/?[^/]+\/modinfo.json$/)
         if (infos.length == 1) {
           my.addModinfo('/download/' + path, infos[0])
           my.resolve()
           return
         }
-        console.warn(path, 'had no modinfo')
+        var infos = zip.file(/modinfo.json$/)
+        if (infos.length > 0) {
+          console.warn(path, infos.length, 'possible misplaced modinfo')
+          infos.forEach(function(file) {console.log('', file.name)})
+        } else {
+          console.warn(path, 'had no modinfo')
+        }
         my.resolve()
       }, function(err) {
         console.warn(path, 'could not be listed', err)
